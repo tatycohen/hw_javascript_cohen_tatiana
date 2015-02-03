ec9f741f2d3c601ec8bec57e80b0d68a9b03ad46
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-02 22:10:35 -0500

    switch to object literal syntax

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-var fortyFour = new Object ();
+var fortyFour = {};
 fortyFour.firstName = "Barack";
 fortyFour.lastName = "Obama";
 fortyFour.termLength = 2;
 fortyFour.party = "Democratic";
 fortyFour.yearsOfPresidency = 8;
 
-var fortyThree = new Object ();
+var fortyThree = {};
 fortyThree.firstName = "George";
 fortyThree.lastName = "Bush";
 fortyThree.termLength = 2;
 fortyThree.party = "Republican";
 fortyThree.yearsOfPresidency = 8;
 
-var fortyTwo = new Object ();
+var fortyTwo = {};
 fortyTwo.firstName = "Bill";
 fortyTwo.lastName = "Clinton";
 fortyTwo.termLength = 2;
 fortyTwo.party = "Democratic";
 fortyTwo.yearsOfPresidency = 8;
 
-var fortyOne = new Object ();
+var fortyOne = {};
 fortyOne.firstName = "George"
 fortyOne.lastName = "Bush"
 fortyOne.termLength = 1;
 fortyOne.party = "Republican";
 fortyOne.yearsOfPresidency = 4;
 
-var forty = new Object ();
+var forty = {};
 forty.firstName = "Ronald";
 forty.lastName = "Reagan";
 forty.termLength = 2;
